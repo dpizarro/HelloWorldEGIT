a667ad217f4773fa7485a16b4a3ec8fd0977e795
Chorla y Victor <juanantonio23cm@hotmail.com>
2012-06-27 17:05:50 +0200

    miercoles

--- src/helloworldegit/HelloWorldEGIT.java
+++ src/helloworldegit/HelloWorldEGIT.java
@@ -3,5 +3,6 @@ package helloworldegit;
 public class HelloWorldEGIT {
 	public static void main( String[] argds ){
 		System.out.println("Hello World from EGIT");
+		System.out.println("miercoles");
 	}
 }
